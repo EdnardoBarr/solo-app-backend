265f152f1dc365a4736869146c1676543c25b26a
1T (RM2-T) Barreto <barreto.lopes@marinha.mil.br>
2024-03-21 16:15:38 -0300

    Creating the packages and classes for User and Role

---
+++ src/main/java/ednardo/api/soloapp/SoloAppApplication.java
@@ -0,0 +1,13 @@
+package ednardo.api.soloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ednardo/api/soloapp/enums/RoleName.java
@@ -0,0 +1,6 @@
+package ednardo.api.soloapp.enums;
+
+public enum RoleName {
+    ROLE_CUSTOMER,
+    ROLE_ADMINISTRATOR
+}
