806bf45217019481d5dfbd455b4cd831b729dab9
Ednardo Barreto <ednardobl@gmail.com>
2024-03-26 23:25:08 -0300

    Created  the Exception handler and the class Generic Response

--- src/main/java/ednardo/api/soloapp/exception/handler/RestResponseExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package ednardo.api.soloapp.exception.handler;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@RestControllerAdvice
-public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
